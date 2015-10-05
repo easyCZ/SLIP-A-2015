b3a45598160c3e63d068d92cd54e8705b7b9057e
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-05 07:57:10 +0100

    Rename config app to project

--- api/src/project/settings.py
+++ api/src/project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'src.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'src.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
