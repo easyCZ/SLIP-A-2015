7995a0722419a8bb8bf4e53d3bcde6481138a41a
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-10 22:14:14 +0100

    Rename subproject

--- api/wsgi/myproject/project/settings.py
+++ api/wsgi/myproject/project/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 # GETTING-STARTED: change 'myproject' to your project name:
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -79,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
