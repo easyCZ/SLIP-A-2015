0b90391de70890ed4562a001602f1e743035fa44
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-10 22:05:49 +0100

    Update descriptions and activate venv

--- api/setup.py
+++ api/setup.py
@@ -4,17 +4,17 @@ from setuptools import setup
 
 setup(
     # GETTING-STARTED: set your app name:
-    name='YourAppName',
+    name='UberVest API',
     # GETTING-STARTED: set your app version:
     version='1.0',
     # GETTING-STARTED: set your app description:
-    description='OpenShift App',
+    description='System Level Integration Project at University of Edinburgh',
     # GETTING-STARTED: set author name (your name):
-    author='Your Name',
+    author='SLIP A 2015',
     # GETTING-STARTED: set author email (your email):
-    author_email='example@example.com',
+    author_email='slip.group.a.2015@gmail.com',
     # GETTING-STARTED: set author url (your url):
-    url='http://www.python.org/sigs/distutils-sig/',
+    url='http://easycz.github.io/SLIP-A-2015/',
     # GETTING-STARTED: define required django version:
     install_requires=[
         'Django==1.8.4'
