c721c875a35b597ba1c9232b9bed8107d5ba92f7
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-13 12:33:50 +0100

    Make api runnable on localhost

--- api/wsgi/src/manage.py
+++ api/wsgi/src/manage.py
@@ -3,8 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
-    # GETTING-STARTED: change 'myproject' to your project name:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+    # Use local config if not on openshift server
+    if os.environ.get('SLIP_ENV') == 'local':
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.local")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
 
     from django.core.management import execute_from_command_line
 
