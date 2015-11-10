042e3e3d944e99a8eb188b8d2026a84636d99652
filipfrahm <filip.frahm@gmail.com>
2015-11-10 13:04:09 +0000

    also added method for extrapolation now. both methods are subject to experiments. I'm aiming to plan the experiments by 11/11.

--- api/bpm/services.py
+++ api/bpm/services.py
@@ -8,14 +8,18 @@ class BPMService(object):
     def __init__(self, data):
         self.data = data
 		        
-	'''# It won't let me call this method.
-	def peak_benchmark(self):
+	'''def avg_volt(self):
 		sum = 0
 		count = 0
 		for time, volts in self.data.items():
 			sum += volts
 			count += 1
 		avg = float(sum)/count
+		return avg
+				
+	# It won't let me call this method.
+	def peak_benchmark(self):
+		avg = self.avg_volt()
 		# There are two options I'd like to test at this point. Comment out one or the other.
 		# 1) PEAK = avg*a, where a is an element of [1.01,1.3)
 		a = 1.05
@@ -24,7 +28,14 @@ class BPMService(object):
 		a = 15
 		peak_benchmark = avg + 15
 		# In both cases I will need to derive an experiment that determines which method is the most accurate. 
-		return peak_benchmark '''
+		return peak_benchmark 
+		
+	def extrapolation_benchmark(self):
+		avg = self.avg_volt()
+		PEAK = self.peak_benchmark()
+		a = 0.3 # subject to experiments
+		extrapolation_benchmark = (PEAK - avg)*a
+		return extrapolation_benchmark'''
 		
     def get_bpm(self):
         peaks = self.get_peaks()
@@ -90,22 +101,34 @@ class BPMService(object):
 ''' # FOR TESTING - importing data and trying the function. It won't let me call it when it's in the class.
 import json
 
+def avg_volt(self):
+		sum = 0
+		count = 0
+		for time, volts in self.data.items():
+			sum += volts
+			count += 1
+		avg = float(sum)/count
+		return avg
+				
+	# It won't let me call this method.
 def peak_benchmark(self):
-	sum = 0
-	count = 0
-	for time, volts in self.data.items():
-		sum += volts
-		count += 1
-	avg = float(sum)/count
+	avg = avg_volt(self)
 	# There are two options I'd like to test at this point. Comment out one or the other.
 	# 1) PEAK = avg*a, where a is an element of [1.01,1.3)
 	a = 1.05
 	peak_benchmark = avg*a
 	# 2) PEAK = avg + a where a is an element of [5,30]
-	# a = 15
-	# peak_benchmark = avg + 15
+	a = 15
+	peak_benchmark = avg + 15
 	# In both cases I will need to derive an experiment that determines which method is the most accurate. 
-	return peak_benchmark
+	return peak_benchmark 
+	
+def extrapolation_benchmark(self):
+	avg = avg_volt(self)
+	PEAK = peak_benchmark(self)
+	a = 0.3 # subject to experiments
+	extrapolation_benchmark = (PEAK - avg)*a
+	return extrapolation_benchmark
 
 def get_json():
 	with open('5sec.json') as f:
@@ -116,4 +139,5 @@ def get_json():
 json_data = get_json()
 window1 = BPMService(json_data)
 PEAK = peak_benchmark(window1)
-print PEAK '''
+EXTRAPOLATION = extrapolation_benchmark(window1)
+print PEAK, EXTRAPOLATION'''
