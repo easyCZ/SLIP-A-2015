dccce048643dfb0e16c6c6fcfde5eb40f9bf5a1d
Milan Pavlik <pavlik.mil@gmail.com>
2015-11-17 22:05:33 +0000

    Fix intendation on bpm service

--- api/bpm/services.py
+++ api/bpm/services.py
@@ -8,33 +8,33 @@ class BPMService(object):
     def __init__(self, data):
         self.data = data
 
-	def avg_volt(self):
-		sum = 0
-		count = 0
-		for time, volts in self.data.items():
-			sum += volts
-			count += 1
-		avg = float(sum)/count
-		return avg
+    def avg_volt(self):
+        sum = 0
+        count = 0
+        for time, volts in self.data.items():
+            sum += volts
+            count += 1
+        avg = float(sum)/count
+        return avg
 
-	# It won't let me call this method.
-	def peak_benchmark(self):
-		avg = self.avg_volt()
-		# There are two options I'd like to test at this point. Comment out one or the other.
-		# 1) PEAK = avg*a, where a is an element of [1.01,1.3)
-		a = 1.05
-		peak_benchmark = avg*a
-		# 2) PEAK = avg + a where a is an element of [5,30]
-		a = 15
-		peak_benchmark = avg + 15
-		# In both cases I will need to derive an experiment that determines which method is the most accurate.
-		return peak_benchmark
+    # It won't let me call this method.
+    def peak_benchmark(self):
+        avg = self.avg_volt()
+        # There are two options I'd like to test at this point. Comment out one or the other.
+        # 1) PEAK = avg*a, where a is an element of [1.01,1.3)
+        a = 1.05
+        peak_benchmark = avg*a
+        # 2) PEAK = avg + a where a is an element of [5,30]
+        a = 15
+        peak_benchmark = avg + 15
+        # In both cases I will need to derive an experiment that determines which method is the most accurate.
+        return peak_benchmark
 
     def extrapolation_benchmark(self):
         avg = self.avg_volt()
         PEAK = self.peak_benchmark()
         a = 0.3 # subject to experiments
-		# extrapolation_benchmark = (PEAK - avg)*a
+        # extrapolation_benchmark = (PEAK - avg)*a
         return extrapolation_benchmark
 
     def get_bpm(self):
@@ -107,39 +107,39 @@ class BPMService(object):
 import json
 
 '''def avg_volt(self):
-		sum = 0
-		count = 0
-		for time, volts in self.data.items():
-			sum += volts
-			count += 1
-		avg = float(sum)/count
-		return avg
+        sum = 0
+        count = 0
+        for time, volts in self.data.items():
+            sum += volts
+            count += 1
+        avg = float(sum)/count
+        return avg
 
-	# It won't let me call this method.
+    # It won't let me call this method.
 def peak_benchmark(self):
-	avg = avg_volt(self)
-	# There are two options I'd like to test at this point. Comment out one or the other.
-	# 1) PEAK = avg*a, where a is an element of [1.01,1.3)
-	a = 1.05
-	peak_benchmark = avg*a
-	# 2) PEAK = avg + a where a is an element of [5,30]
-	a = 15
-	peak_benchmark = avg + 15
-	# In both cases I will need to derive an experiment that determines which method is the most accurate.
-	return peak_benchmark
+    avg = avg_volt(self)
+    # There are two options I'd like to test at this point. Comment out one or the other.
+    # 1) PEAK = avg*a, where a is an element of [1.01,1.3)
+    a = 1.05
+    peak_benchmark = avg*a
+    # 2) PEAK = avg + a where a is an element of [5,30]
+    a = 15
+    peak_benchmark = avg + 15
+    # In both cases I will need to derive an experiment that determines which method is the most accurate.
+    return peak_benchmark
 
 def extrapolation_benchmark(self):
-	avg = avg_volt(self)
-	PEAK = peak_benchmark(self)
-	a = 0.3 # subject to experiments
-	extrapolation_benchmark = (PEAK - avg)*a
-	return extrapolation_benchmark'''
+    avg = avg_volt(self)
+    PEAK = peak_benchmark(self)
+    a = 0.3 # subject to experiments
+    extrapolation_benchmark = (PEAK - avg)*a
+    return extrapolation_benchmark'''
 
 def get_json():
-	with open('5sec.json') as f:
-		json_data = json.loads(f.read())
-		return json_data
-	return {}
+    with open('5sec.json') as f:
+        json_data = json.loads(f.read())
+        return json_data
+    return {}
 
 json_data = get_json()
 window1 = BPMService(json_data)
