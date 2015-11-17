99e9fa68ef3b74a0756d87971268ad2cae5088ce
filipfrahm <filip.frahm@gmail.com>
2015-11-17 16:16:19 +0000

    writing functions neccessary to do gaussian

--- respiratory/Processed Datasets/Processed Datasets/matrix.py
+++ respiratory/Processed Datasets/Processed Datasets/matrix.py
@@ -68,9 +68,55 @@ class matrix(object):
 		self = matrix(self.rows)
 		return self
 	
+	# adding i*factor to k
+	def row_add(self,i,k,factor):
+		rowi_times_factor = scalar_multiply_list(factor, self.row(i))
+		new_row = add_list(rowi_times_factor,self.row(k))
+		self.change_row(k,new_row)
+		return self
+
+	def scalar(self,scalar):
+		for i in range(1,A.row_dim+1):
+			for j in range(1,A.col_dim+1):
+				A.change_element(i,j,scalar*A.element(i,j))
+		return self
+
 def transpose(A):
 	A_transpose = matrix(A.cols)
 	return A_transpose
+
+def add(A,B):
+	row_dim = A.row_dim
+	col_dim = A.col_dim
+	A_plus_B = matrix(zeros(row_dim,col_dim))
+	if A.row_dim <> B.row_dim or A.col_dim <> B.row_dim:
+		return "ERROR"
+	else:
+		for i in range(1,row_dim+1):
+			for j in range(1,col_dim+1):
+				A_plus_B.change_element(i,j,A.element(i,j)+B.element(i,j))
+	return A_plus_B
+
+def add_list(A,B):
+	A = [A]
+	B = [B]
+	A = matrix(A)
+	B = matrix(B)
+	A_plus_B = add(A,B)
+	return A_plus_B.rows
+
+def scalar_multiply(scalar,A):
+	scalar_A = matrix(A.rows)
+	for i in range(1,A.row_dim+1):
+		for j in range(1,A.col_dim+1):
+			scalar_A.change_element(i,j,scalar*A.element(i,j))
+	return scalar_A
+
+def scalar_multiply_list(scalar,A):
+	A = [A]
+	A = matrix(A)
+	A.scalar(scalar)
+	return A.rows
 
 def multiply(A,B):
 	row_dim = A.row_dim
@@ -117,10 +163,12 @@ def LU_decomp(A): # by construction
 
 A = matrix(Test)
 A_transpose = transpose(A)
-# I = identity(A.row_dim)
+I = identity(A.row_dim)
+I.row_add(5,6,8)
+I.matrix_print()
 # A.matrix_print()
 AB = multiply(A_transpose,A)
 # AB.matrix_print()
-LU = LU_decomp(AB)
-LU[0].matrix_print()
-LU[1].matrix_print()
+# LU = LU_decomp(AB)
+# LU[0].matrix_print()
+# LU[1].matrix_print()
