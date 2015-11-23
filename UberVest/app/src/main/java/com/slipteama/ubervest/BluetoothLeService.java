fed888c728cfdac1fd17a6460b57a4c060023ccc
Jon Redmond <jonredmond94@gmail.com>
2015-11-23 12:24:49 +0000

    Fixed connection time issues and App loads log in page after connecting

--- UberVest/app/src/main/java/com/slipteama/ubervest/BluetoothLeService.java
+++ UberVest/app/src/main/java/com/slipteama/ubervest/BluetoothLeService.java
@@ -1,6 +1,5 @@
 package com.slipteama.ubervest;
 
-import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
@@ -8,68 +7,75 @@ import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
-import android.content.Intent;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 
-import android.os.Binder;
-import android.os.IBinder;
+import android.os.AsyncTask;
+
+import android.os.ParcelUuid;
 import android.util.Log;
 
 import com.firebase.client.Firebase;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.Random;
+import java.util.List;
 import java.util.UUID;
 
 /**
  * Created by jon on 20/10/15.
  */
-public class BluetoothLeService extends Service {
+public class BluetoothLeService extends AsyncTask<Void, Void, Void> {
     private final static String TAG = BluetoothLeService.class.getSimpleName();
 
-    private BluetoothManager mBluetoothManager;
-    private BluetoothAdapter mBluetoothAdapter;
     private BluetoothGatt mBluetoothGatt;
+    private BluetoothAdapter btAdapter;
+    private BluetoothDevice btDevice;
+    private BluetoothLeScanner btScanner;
 
-    private String mBluetoothDeviceAddress;
+    public static Context mContext;
 
     private static UUID UBER_VEST_SERVICE_UUID = UUID.fromString("0000B000-0000-1000-8000-00805F9B34FB");
 
     private static UUID ECG_CHARACTERISTIC_UUID = UUID.fromString("0000B002-0000-1000-8000-00805F9B34FB");
 
-    private int mConnectionState = STATE_DISCONNECTED;
-
-    private static final int STATE_DISCONNECTED = 0;
-    private static final int STATE_CONNECTING = 1;
-    private static final int STATE_CONNECTED = 2;
-
     private Firebase firebase = new Firebase("https://ubervest.firebaseio.com/");
+
+    private ScanCallback leScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            if(result.getDevice().getAddress().equals("DB:00:EC:05:2E:A0")) {
+                btDevice = result.getDevice();
+                mBluetoothGatt = btDevice.connectGatt(mContext, false, mGattCallback);
+            }
+        }
+    };
 
     private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
         @Override
         public void onCharacteristicChanged(BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic) {
-            byte[] data = characteristic.getValue();
-            ByteBuffer wrapper = ByteBuffer.wrap(data);
-            firebase.child("devices").child("0").child("raw_ecg").setValue(wrapper.getInt(0));
+            Integer data = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 0);
+
+            Log.i(TAG, data.toString());
+
+            long currentTime = (new Date()).getTime();
+            firebase.child("devices").child("0").child("raw_ecg").child(currentTime + "").setValue(data);
         }
 
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
             if (newState == BluetoothProfile.STATE_CONNECTED) {
-                mConnectionState = STATE_CONNECTED;
                 mBluetoothGatt.discoverServices();
                 Log.i(TAG, "Connected to GATT server.");
                 // Attempts to discover services after successful connection.
                 Log.i(TAG, "Attempting to start service discovery:" +
                         mBluetoothGatt.discoverServices());
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                mConnectionState = STATE_DISCONNECTED;
             }
         }
 
@@ -78,80 +84,26 @@ public class BluetoothLeService extends Service {
                 BluetoothGattService service = gatt.getService(UBER_VEST_SERVICE_UUID);
                 BluetoothGattCharacteristic chara = service.getCharacteristic(ECG_CHARACTERISTIC_UUID);
                 gatt.setCharacteristicNotification(chara, true);
+                for (BluetoothGattDescriptor desc : chara.getDescriptors()) {
+                    desc.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                    gatt.writeDescriptor(desc);
+                }
             }
         }
     };
 
-    public class LocalBinder extends Binder {
-        BluetoothLeService getService() {
-            return BluetoothLeService.this;
-        }
-    }
-
     @Override
-    public IBinder onBind(Intent intent) {
-        return mBinder;
+    protected Void doInBackground(Void... params) {
+        ScanFilter.Builder filterBuilder = new ScanFilter.Builder();
+        filterBuilder.setServiceUuid(new ParcelUuid(UBER_VEST_SERVICE_UUID));
+        ScanFilter filter = filterBuilder.build();
+        List<ScanFilter> filterList = new ArrayList<ScanFilter>();
+        filterList.add(filter);
+        ScanSettings.Builder settingsBuilder = new ScanSettings.Builder();
+        //settingsBuilder.setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE);
+        settingsBuilder.setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY);
+        ScanSettings settings = settingsBuilder.build();
+        btScanner.startScan(filterList, settings, leScanCallback);
+        return null;
     }
-
-    @Override
-    public boolean onUnbind(Intent intent) {
-        close();
-        return super.onUnbind(intent);
-    }
-
-    private final IBinder mBinder = new LocalBinder();
-
-    public boolean initialize() {
-        if (mBluetoothManager == null) {
-            mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-            if (mBluetoothManager == null) {
-                Log.e(TAG, "Unable to initialize BluetoothManager.");
-                return false;
-            }
-        }
-        mBluetoothAdapter = mBluetoothManager.getAdapter();
-        if (mBluetoothAdapter == null) {
-            Log.e(TAG, "Unable to obtain a BluetoothAdapter.");
-            return false;
-        }
-        return true;
-    }
-
-
-    public boolean connect(final String address) {
-        if (mBluetoothAdapter == null || address == null) {
-            Log.w(TAG, "BluetoothAdapter not initialized or unspecified address.");
-            return false;
-        }
-        
-        final BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
-        if (device == null) {
-            Log.w(TAG, "Device not found.  Unable to connect.");
-            return false;
-        }
-        // We want to directly connect to the device, so we are setting the autoConnect
-        // parameter to false.
-        mBluetoothGatt = device.connectGatt(this, false, mGattCallback);
-        Log.d(TAG, "Trying to create a new connection.");
-        mBluetoothDeviceAddress = address;
-        mConnectionState = STATE_CONNECTING;
-        return true;
-    }
-
-    public void disconnect() {
-        if (mBluetoothAdapter == null || mBluetoothGatt == null) {
-            Log.w(TAG, "BluetoothAdapter not initialized");
-            return;
-        }
-        mBluetoothGatt.disconnect();
-    }
-
-    public void close() {
-        if (mBluetoothGatt == null) {
-            return;
-        }
-        mBluetoothGatt.close();
-        mBluetoothGatt = null;
-    }
-
 }
